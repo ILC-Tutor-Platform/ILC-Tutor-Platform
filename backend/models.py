b1f5c76e4db479aec386a8870ba443d8d27ab879
errolJames59 <erroljamesminguez59@gmail.com>
2025-04-04 20:54:23 +0800

    feat: add user role systems

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,57 @@
 # Description: This file contains the ORM model for the UserDetail table that will be used to create the table in the database.
 
-from sqlalchemy import Column, Integer, Date, String
+import uuid
+from sqlalchemy import Column, Integer, Date, String, ForeignKey, text
 from database import Base, engine  # Directly import engine
+from sqlalchemy.dialects.postgresql import UUID
 
 # Define ORM model
 class UserDetail(Base):
     __tablename__ = "user_detail"
 
-    userid = Column(Integer, primary_key=True, index=True)
+    userid = Column(UUID(as_uuid=True), primary_key=True, index=True, server_default=text("gen_random_uuid()")) # added server_default="gen_random_uuid()" to auto generate a uuid
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     role = Column(String, nullable=False)
     datejoined = Column(Date, nullable=False)
+
+class AdminDetail(Base):
+    __tablename__ = "admin_detail"
+
+    admin_id = Column(UUID(as_uuid=True), ForeignKey("user_detail.userid"), primary_key=True, index=True)
+    admin_role = Column(String, nullable=False)
+
+class StudentDetail(Base):
+    __tablename__ = "student_detail"
+
+    student_id = Column(UUID(as_uuid=True), ForeignKey("user_detail.userid"), primary_key=True, index=True)
+    student_number = Column(String, unique=True, index=True, nullable=False)
+    interests = Column(String, nullable=False)
+    degree_program = Column(String, nullable=False)
+
+class TutorDetail(Base):
+    __tablename__ = "tutor_detail"
+
+    tutor_id = Column(UUID(as_uuid=True), ForeignKey("user_detail.userid"), primary_key=True, index=True)
+    expertise = Column(String, nullable=False)
+    subject_id = Column(UUID(as_uuid=True), ForeignKey("subject.subject_id"), nullable=False)
+    availability = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    social_media_links = Column(String, nullable=False)
+    status = Column(UUID(as_uuid=True), ForeignKey("status_detail.status_id"), nullable=False)
+
+class StatusDetail(Base):
+    __tablename__ = "status_detail"
+
+    status_id = Column(UUID(as_uuid=True), primary_key=True, index=True, server_default=text("gen_random_uuid()")) # added server_default="gen_random_uuid()" to auto generate a uuid
+    status = Column(String, nullable=False)
+
+class SubjectDetail(Base):
+    __tablename__ = "subject"
+
+    subject_id = Column(UUID(as_uuid=True), primary_key=True, index=True, server_default=text("gen_random_uuid()")) # added server_default="gen_random_uuid()" to auto generate a uuid
+    subject_name = Column(String, nullable=False)
+    subject_description = Column(String, unique=True, index=True, nullable=False)
 
 # Create Tables
 print("🚀 Attempting to create tables in Supabase...")
--- backend/schema.py
+++ backend/schema.py
@@ -11,3 +11,41 @@ class UserDetailSchema(BaseModel):
 
     class Config:
         from_attributes = True  
+
+class AdminDetailSchema(BaseModel):
+    admin_role: str
+
+    class Config:
+        from_attributes = True
+
+class StudentDetailSchema(BaseModel):
+    student_number: str
+    interests: str
+    degree_program: str
+
+    class Config:
+        from_attributes = True
+
+class TutorDetailSchema(BaseModel):
+    expertise: str
+    subject_id: str
+    availability: str
+    description: str
+    social_media_links: str
+    status: str
+
+    class Config:
+        from_attributes = True
+
+class StatusDetailSchema(BaseModel):
+    status: str
+
+    class Config:
+        from_attributes = True
+
+class SubjectDetailSchema(BaseModel):
+    subject_name: str
+    subject_description: str
+
+    class Config:
+        from_attributes = True
