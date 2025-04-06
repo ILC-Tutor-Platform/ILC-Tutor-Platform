28b9aa5b8288dd9a77a46b5b4404c7869aa0903f
pixie1013 <152234323+pixie1013@users.noreply.github.com>
2025-04-06 16:45:52 +0800

    chore: restructured files

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 # Description: This file contains the database connection and session creation logic.
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
@@ -7,6 +7,14 @@ import os
 
 # Initialize Metadata and Base
 load_dotenv()
+
+# Dependency to get a session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # Provide session to request
+    finally:
+        db.close()  # Ensure session is closed after request
 
 def db_connect():
 
