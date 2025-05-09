71bf08f8c4b5485549a4c5ad752ed827ed8af867
ejminguez <ebminguez@up.edu.ph>
2025-05-09 19:32:35 +0800

    feat: get_tutors and get_tutor_by_id

--- backend/models/tutor/socials.py
+++ backend/models/tutor/socials.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, UUID, ForeignKey, String
 from models import Base
 import uuid
+from sqlalchemy.orm import relationship
 
 class TutorSocials(Base):
     __tablename__ = "tutor_socials"
 
     tutor_socials_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    tutor_id = Column(UUID(as_uuid=True), ForeignKey("tutor_detail.tutor_id"))
+    tutor_id = Column(UUID(as_uuid=True), ForeignKey("user_detail.userid"))
     socials = Column(String, nullable=True)
+
+    user = relationship("UserDetail", back_populates="tutor_socials")
--- backend/models/user/user_detail.py
+++ backend/models/user/user_detail.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Date, String
 from models import Base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+from sqlalchemy.orm import relationship
 
 # Define ORM model
 class UserDetail(Base):
@@ -12,3 +13,10 @@ class UserDetail(Base):
     name = Column(String, nullable=True)
     email = Column(String, unique=True, index=True, nullable=False)
     datejoined = Column(Date, nullable=False)
+
+    # Relationships - tutor and user_detail
+    tutor_detail = relationship("TutorDetail", back_populates="user", uselist=False)
+    tutor_affiliation = relationship("TutorAffiliation", back_populates="user", uselist=False)
+    tutor_availability = relationship("TutorAvailability", back_populates="user", uselist=False)
+    tutor_expertise = relationship("TutorExpertise", back_populates="user", uselist=False)
+    tutor_socials = relationship("TutorSocials", back_populates="user", uselist=False)
