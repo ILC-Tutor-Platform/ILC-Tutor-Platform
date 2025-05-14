ac455bc1224841800bf2b1046c14dd07eec70c1a
ejminguez <ebminguez@up.edu.ph>
2025-05-15 06:08:41 +0800

    feat: admin sign in handler

--- frontend/src/pages/SigninAsAdmin.tsx
+++ frontend/src/pages/SigninAsAdmin.tsx
@@ -15,22 +15,108 @@ const SigninAsAdmin = () => {
   }>({});
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const validateFields = () => {
+    const newErrors: {
+      email?: string;
+      password?: string;
+      invalidCredentials?: string;
+    } = {};
+
+    if (!email.trim()) {
+      newErrors.email = '*Email is required.';
+    }
+    if (!password.trim()) newErrors.password = '*Password is required.';
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSignIn = async () => {
+    setLoading(true);
+    if (!validateFields()) {
+      setLoading(false);
+      return;
+    }
+
+    try {
+      console.log('email', email);
+      console.log('password', password);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1200);
+    } catch (error) {
+      console.error('Error signing in:', error);
+      setErrors({ invalidCredentials: 'Invalid email or password.' });
+    }
+  };
+
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <form className="flex flex-col w-[90%] gap-4 md:gap-10 xl:w-[30%] mx-auto py-7 md:px-10 rounded-2xl green-shadow-card p-8">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSignIn();
+        }}
+        className="flex flex-col w-[90%] gap-4 md:gap-10 md:w-[50%] xl:w-[30%] mx-auto py-7 md:px-10 rounded-2xl green-shadow-card p-8"
+      >
         <div className="grid gap-10">
           <img src={Logo} alt="Logo" className="w-35 h-auto mx-auto" />
           <h2 className="font-bold text-5xl text-center">Admin Sign in</h2>
         </div>
-        <div>
-          <Label>Email:</Label>
-          <Input type="email" id="email" name="email" required />
+        <div className="flex flex-col gap-5">
+          <div>
+            {errors.email && (
+              <Label className="text-[0.8rem] font-thin text-red-500">
+                {errors.email}
+              </Label>
+            )}
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email address"
+              className={`p-3 mt-2 ${errors.email ? 'border-red-500' : ''}`}
+            />
+          </div>
+          <div>
+            <div>
+              {errors.password && (
+                <Label className="text-[0.8rem] font-thin text-red-500">
+                  {errors.password}
+                </Label>
+              )}
+              <Input
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                className={`p-3 mt-2 ${errors.password ? 'border-red-500' : ''}`}
+              />
+            </div>
+            <div className="flex items-center mt-2">
+              <input
+                type="checkbox"
+                id="showPassword"
+                checked={showPassword}
+                onChange={handleShowPassword}
+                className="mr-2"
+              />
+              <label htmlFor="showPassword" className="text-sm text-ilc-grey">
+                Show Password
+              </label>
+            </div>
+          </div>
         </div>
-        <div>
-          <Label>Password:</Label>
-          <Input type="password" id="password" name="password" required />
-        </div>
-        <Button type="submit" variant={'yellow-button'}>
+        <Button type="submit" disabled={loading} variant={'yellow-button'}>
           Sign In as Admin
         </Button>
 
