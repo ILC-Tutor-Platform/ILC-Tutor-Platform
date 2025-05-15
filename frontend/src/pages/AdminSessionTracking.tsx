fa8492190f30e9e36d0e1e0f20f12b35e47e9121
ejminguez <ebminguez@up.edu.ph>
2025-05-16 02:02:19 +0800

    lint: npm run lint:fix

--- frontend/src/pages/AdminSessionTracking.tsx
+++ frontend/src/pages/AdminSessionTracking.tsx
@@ -99,12 +99,13 @@ const AdminSessionTracking = () => {
                   <div>{schedule.subject}</div>
                   <div>
                     <span
-                      className={`border text-xs px-3 py-1 rounded-full ${schedule.status === 'APPROVED'
+                      className={`border text-xs px-3 py-1 rounded-full ${
+                        schedule.status === 'APPROVED'
                           ? 'border-[#307B74] text-[#307B74]'
                           : schedule.status === 'DECLINED'
                             ? 'border-[#8A1538] text-[#8A1538]'
                             : 'border-gray-400 text-gray-500'
-                        }`}
+                      }`}
                     >
                       {schedule.status}
                     </span>
